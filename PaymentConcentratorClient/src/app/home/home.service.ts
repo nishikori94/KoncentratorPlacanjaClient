fb431f8b4a8cfaedde5180576e6356d58ce4ddd0
Milan <mgvero94@gmail.com>
2019-01-03 17:14:42 +0100

    HTTPS

--- PaymentConcentratorClient/src/app/home/home.service.ts
+++ PaymentConcentratorClient/src/app/home/home.service.ts
@@ -23,10 +23,11 @@ export class HomeService {
   constructor(private http: HttpClient) { }
 
   getPaymentMethods(merchantId: string): Observable<PaymentMethod[]> {
-      return this.http.get<PaymentMethod[]>(`http://localhost:9091/placanje/tipoviPlacanja/` + merchantId);
+      return this.http.get<PaymentMethod[]>(`https://localhost:9091/placanje/tipoviPlacanja/` + merchantId);
   }
 
   makePayment(url: string, merchantOrderId: string): Observable<UrlResponse> {
+    console.log(url);
     return this.http.get<UrlResponse>(url + "/" + merchantOrderId);
   }
 }
